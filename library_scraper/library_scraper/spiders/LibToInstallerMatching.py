2dee67c1e74b3e440abafcb7b7b2492fef441c0a
mgdoughe <mgdoughe@ncsu.edu>
2020-09-10 22:39:49 -0400

    Subbed XPath for bsoup with regex
    Works much better

--- library_scraper/library_scraper/spiders/LibToInstallerMatching.py
+++ library_scraper/library_scraper/spiders/LibToInstallerMatching.py
@@ -1,21 +1,16 @@
 import scrapy
+from bs4 import BeautifulSoup
+import re
 
 class QuotesSpider(scrapy.Spider):
     name = "keywords"
     start_urls = [
         'https://github.com/PetrochukM/PyTorch-NLP',
+        'https://github.com/benedekrozemberczki/karateclub'
     ]
 
     def parse(self, response):
-        pip_siblings = response.xpath('//*[text()="pip"]/following-sibling::span/text()').getall()
-        pip_keyword = [""]
-        keyword_count = 0
-        for word in pip_siblings:
-            if word == "install":
-                keyword_count += 1
-                if keyword_count > 1: #TODO add support for multiple pip keywords 
-                    break
-                continue
-            pip_keyword[0] += word
-
-        print(pip_keyword)
+        soup = BeautifulSoup(response.body, features="lxml")
+        text_of_soup = soup.get_text().strip()
+        regex_results = re.search("pip install ([a-zA-Z\-]+)", text_of_soup).group()
+        print(regex_results) #TODO add support for multiple pip keywords 
