5242964274594453c3b650547252578ca49a96bd
mgdoughe <mgdoughe@ncsu.edu>
2020-09-16 22:01:05 -0400

    Trying to construct spider

--- library_scraper/library_scraper/spiders/LibToInstallerMatching.py
+++ library_scraper/library_scraper/spiders/LibToInstallerMatching.py
@@ -5,12 +5,17 @@ import re
 class QuotesSpider(scrapy.Spider):
     name = "keywords"
     start_urls = [
-        'https://github.com/PetrochukM/PyTorch-NLP',
-        'https://github.com/benedekrozemberczki/karateclub'
+        #'https://github.com/PetrochukM/PyTorch-NLP',
+        #'https://github.com/benedekrozemberczki/karateclub'
     ]
+
+    def __init__(self, start_url, *args, **kwargs):
+        super(QuotesSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [start_url]
 
     def parse(self, response):
         soup = BeautifulSoup(response.body, features="lxml")
-        text_of_soup = soup.get_text().strip()
+        text_of_soup = soup.get_text().strip() # turn html into text 
         regex_results = re.search("pip install ([a-zA-Z\-]+)", text_of_soup).group()
         print(regex_results) #TODO add support for multiple pip keywords 
+        return regex_results
--- parser/markdown.py
+++ parser/markdown.py
@@ -1,6 +1,12 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import os
+os.system("python ../library_scraper/library_scraper/spiders/LibToInstallerMatching.py")
+from LibToInstallerMatching import QuotesSpider
+
 if __name__ == '__main__':
     
-    file_path = "C:/Users/slo4/Downloads/awesome-python-master/awesome-python-master/README.md"
+    file_path = "source_data/awesome-python-master-README.md"
     #file_path = "/Users/personal/Downloads/awesome-python-master/README.md"
 
     my_dict = {}
@@ -29,12 +35,14 @@ if __name__ == '__main__':
                 values.append(words[1])
     newList = tuple(values)
     my_dict[key] = newList
+    #print(my_dict)
 
+    process = CrawlerProcess(settings={})
 
-            
-                    
-
-
-  
-
-
+    for category in my_dict:
+        for address in my_dict[category]:
+            address = address[address.find('(')+1:-1]
+            if address.find('http') != -1:
+                process.crawl(QuotesSpider, start_url=address)
+                process.start() # the script will block here until the crawling is finished
+            print(address)
