2d7920a5b0e37d2189488ec1b51d8d18ee63caf2
Sabrina Lo <slo4@ncsu.edu>
2020-10-12 11:49:10 -0400

    revert back db/deploy/python.sql file that was accidentally changed

--- db/deploy/python.sql
+++ db/deploy/python.sql
@@ -1,62 +1,32 @@
--- Verify awesome:python on pg
+-- Deploy awesome:python to pg
 
 BEGIN;
 
-SELECT pg_catalog.has_table_privilege('api.awesome_python', 'select');
+CREATE SERVER awesome_list FOREIGN DATA WRAPPER multicorn OPTIONS (
+    wrapper 'awesome.alist.ForeignDataWrapper'
+);
 
-SELECT pg_catalog.has_table_privilege('api.awesome_python_categories', 'select');
+CREATE SERVER awesome_list_categories FOREIGN DATA WRAPPER multicorn OPTIONS (
+    wrapper 'awesome.alist.Categories'
+);
 
-SELECT category_slug from api.awesome_python;
+CREATE FOREIGN TABLE api.awesome_python (
+    "category_slug" CHARACTER VARYING,
+    "name" CHARACTER VARYING,
+    "fqn" CHARACTER VARYING,
+    "url" CHARACTER VARYING
+) SERVER awesome_list OPTIONS (
+    repo 'vinta/awesome-python'
+);
 
-SELECT name from api.awesome_python;
+CREATE FOREIGN TABLE api.awesome_python_categories (
+    "name" CHARACTER VARYING,
+    "slug" CHARACTER VARYING
+) SERVER awesome_list_categories OPTIONS (
+    repo 'vinta/awesome-python'
+);
 
-SELECT fqn from api.awesome_python;
+GRANT SELECT ON api.awesome_python TO web_anon;
+GRANT SELECT ON api.awesome_python_categories TO web_anon;
 
-SELECT url from api.awesome_python;
-
-SELECT name from api.awesome_python_categories;
-
-SELECT slug from api.awesome_python_categories;
-
-
-DO $$
-DECLARE
-    n varchar;
-    u varchar;
-    f varchar;
-    category varchar;
-    num int;
-BEGIN
-   n := (SELECT name FROM api.awesome_python WHERE url = 'https://github.com/ajenti/ajenti');
-   ASSERT n = 'ajenti';
-   u := (SELECT url FROM api.awesome_python WHERE name = 'trio');
-   ASSERT u = 'https://github.com/python-trio/trio';
-   category := (SELECT category_slug FROM api.awesome_python WHERE url = 'https://github.com/lepture/authlib');
-   ASSERT category = 'authentication';
-   f := (SELECT fqn FROM api.awesome_python WHERE url = 'https://github.com/prabhupant/python-ds');
-   ASSERT f = 'python-ds';
-   n := (SELECT name FROM api.awesome_python WHERE category_slug  = 'cms' AND url = 'https://github.com/feincms/feincms');
-   ASSERT n = 'feincms';
-   u := (SELECT url FROM api.awesome_python WHERE name = 'django-cache-machine' AND category_slug = 'caching');
-   ASSERT u = 'https://github.com/django-cache-machine/django-cache-machine';
-   f := (SELECT fqn FROM api.awesome_python WHERE name = 'eyeD3' AND category_slug = 'audio');
-   ASSERT f = 'eyed3';
-   category := (SELECT category_slug FROM api.awesome_python WHERE name = 'pylint' AND url = 'https://www.pylint.org/');
-   ASSERT category = 'code-analysis';
-   num := (SELECT COUNT(*) FROM api.awesome_python WHERE category_slug = 'code-analysis');
-   ASSERT num = 18;
-END $$;
-
-
-DO $$
-DECLARE
-    n varchar;
-    s varchar;
-BEGIN
-   n := (SELECT name FROM api.awesome_python_categories WHERE slug = 'admin-panels');
-   ASSERT n = 'Admin Panels';
-   s := (SELECT slug FROM api.awesome_python_categories WHERE name = 'Built-in Classes Enhancement');
-   ASSERT s = 'built-in-classes-enhancement';
-END $$;
-
-ROLLBACK;
+COMMIT;
