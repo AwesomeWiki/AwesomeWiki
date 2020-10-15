df66821afb8ad6b3339811bf57214f229a6cde7d
Sabrina Lo <slo4@sd-vm03.csc.ncsu.edu>
2020-10-15 17:02:51 -0400

    Complete the verify script with testing for api.python_package

--- db/verify/python.sql
+++ db/verify/python.sql
@@ -4,8 +4,6 @@ BEGIN;
 
 SELECT pg_catalog.has_table_privilege('api.awesome_python', 'select');
 
-SELECT pg_catalog.has_table_privilege('api.awesome_python_categories', 'select');
-
 SELECT category_slug from api.awesome_python;
 
 SELECT name from api.awesome_python;
@@ -13,10 +11,6 @@ SELECT name from api.awesome_python;
 SELECT fqn from api.awesome_python;
 
 SELECT url from api.awesome_python;
-
-SELECT name from api.awesome_python_categories;
-
-SELECT slug from api.awesome_python_categories;
 
 
 DO $$
@@ -48,6 +42,12 @@ BEGIN
 END $$;
 
 
+SELECT pg_catalog.has_table_privilege('api.awesome_python_categories', 'select');
+
+SELECT name from api.awesome_python_categories;
+
+SELECT slug from api.awesome_python_categories;
+
 DO $$
 DECLARE
     n varchar;
@@ -59,4 +59,42 @@ BEGIN
    ASSERT s = 'built-in-classes-enhancement';
 END $$;
 
+
+SELECT pg_catalog.has_table_privilege('api.python_package', 'select');
+
+SELECT pg_catalog.has_table_privilege('api.python_package', 'select');
+
+SELECT category_slug from api.awesome_python;
+
+SELECT name from api.awesome_python;
+
+SELECT fqn from api.awesome_python;
+
+SELECT url from api.awesome_python;
+
+
+DO $$
+DECLARE
+    n varchar;
+    u varchar;
+    s varchar;
+    i varchar;
+BEGIN
+   n := (SELECT name from api.python_package where fqn = 'ajenti');
+   ASSERT n = 'ajenti';
+   u := (SELECT url FROM api.python_package WHERE fqn = 'trio');
+   ASSERT u = 'https://github.com/python-trio/trio';
+   s := (SELECT category_slug FROM api.python_package WHERE fqn = 'authlib');
+   ASSERT s = 'authentication';
+    i := (SELECT info ->> 'info' AS info  from api.python_package where fqn = 'ajenti');
+    ASSERT i = '{"author": "Eugeny Pankov", "author_email": "e@ajenti.org", "bugtrack_url": null, "classifiers": [], "description": "", "description_content_type": null, "docs_url": null, "download_url": "", "downloads": {"last_day": -1, "last_month": -1, "last_week": -1}, "home_page": "http://ajenti.org/", "keywords": "", "license": "", "maintainer": "", "maintainer_email": "", "name": "ajenti", "package_url": "https://pypi.org/project/ajenti/", "platform": "", "project_url": "https://pypi.org/project/ajenti/", "project_urls": {"Homepage": "http://ajenti.org/"}, "release_url": "https://pypi.org/project/ajenti/1.2.23.13/", "requires_dist": null, "requires_python": "", "summary": "The server administration panel", "version": "1.2.23.13", "yanked": false, "yanked_reason": null}';
+   n := (SELECT name FROM api.python_package WHERE fqn  = 'feincms' AND url = 'https://github.com/feincms/feincms');
+   ASSERT n = 'feincms';
+   u := (SELECT url FROM api.python_package WHERE fqn = 'django-cache-machine' AND category_slug = 'caching');
+   ASSERT u = 'https://github.com/django-cache-machine/django-cache-machine';
+   i := (SELECT info ->> 'info' AS info  from api.python_package where fqn = 'ajenti' and name = 'ajenti');
+   ASSERT  i= '{"author": "Eugeny Pankov", "author_email": "e@ajenti.org", "bugtrack_url": null, "classifiers": [], "description": "", "description_content_type": null, "docs_url": null, "download_url": "", "downloads": {"last_day": -1, "last_month": -1, "last_week": -1}, "home_page": "http://ajenti.org/", "keywords": "", "license": "", "maintainer": "", "maintainer_email": "", "name": "ajenti", "package_url": "https://pypi.org/project/ajenti/", "platform": "", "project_url": "https://pypi.org/project/ajenti/", "project_urls": {"Homepage": "http://ajenti.org/"}, "release_url": "https://pypi.org/project/ajenti/1.2.23.13/", "requires_dist": null, "requires_python": "", "summary": "The server administration panel", "version": "1.2.23.13", "yanked": false, "yanked_reason": null}';
+END $$;
+
 ROLLBACK;
+
