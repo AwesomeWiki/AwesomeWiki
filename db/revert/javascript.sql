54a802ca96c73d66688992dcfaa5d1dda62fb052
Sabrina Lo <slo4@ncsu.edu>
2020-10-30 13:39:34 -0400

    add javascript to the revert folder

---
+++ db/revert/javascript.sql
@@ -0,0 +1,13 @@
+-- Revert awesome:python from pg
+
+BEGIN;
+
+DROP FOREIGN TABLE IF EXISTS api.awesome_javascript_categories;
+DROP FOREIGN TABLE IF EXISTS api.awesome_javascript;
+DROP FOREIGN TABLE IF EXISTS api.javascript_package;
+
+DROP SERVER IF EXISTS awesome_list_categories;
+DROP SERVER IF EXISTS awesome_list;
+DROP SERVER IF EXISTS javascript_package;
+
+COMMIT;
