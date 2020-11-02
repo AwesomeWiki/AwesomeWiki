6c488dd5bd42eb74ade05ecbcf41b8dc3b030b7e
Sabrina Lo <slo4@ncsu.edu>
2020-11-02 14:17:59 -0500

    remove some comments and merge Sam's code from master

--- db/deploy/javascript.sql
+++ db/deploy/javascript.sql
@@ -2,14 +2,6 @@
 
 BEGIN;
 
--- CREATE SERVER IF NOT EXISTS awesome_list FOREIGN DATA WRAPPER multicorn OPTIONS (
---     wrapper 'awesome.alist.ForeignDataWrapper'
--- );
-
--- CREATE SERVER IF NOT EXISTS awesome_list_categories FOREIGN DATA WRAPPER multicorn OPTIONS (
---     wrapper 'awesome.alist.Categories'
--- );
-
 CREATE SERVER IF NOT EXISTS javascript_package FOREIGN DATA WRAPPER multicorn OPTIONS (
     wrapper 'awesome.javascript.Package'
 );
