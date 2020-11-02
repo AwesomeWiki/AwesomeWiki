af8d1a688ae830fc97530bb09e7850c18e4f8c18
Sabrina Lo <slo4@sd-vm03.csc.ncsu.edu>
2020-11-02 15:02:55 -0500

    debugging api.javascript_package

--- fdws/awesome/javascript.py
+++ fdws/awesome/javascript.py
@@ -13,11 +13,17 @@ from markdown import getPackageName
 def findPackageFromNPM(package):
     try:
         url = 'https://api.npms.io/v2/package/' + package
+        with open('/tmp/awesome_py_log0', 'w') as f:
+            print(url, file=f)
         ##This is from https://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        with open('/tmp/awesome_py_log1', 'w') as f:
+            print("Mozilla", file=f)
         web_byte = urlopen(req).read()
         webpage = web_byte.decode('utf-8')
         result1 = json.loads(webpage)
+        with open('/tmp/awesome_py_log2', 'w') as f:
+            print(result1, file=f)
         result1.pop('analyzedAt')
         info = json.dumps(result)
     except:
